9e7f1f503b259c2365a9cee17544eaddf4e2f175
Dezyop2 <wlebouvier@normandiewebschool.fr>
2023-10-17 15:05:00 +0200

    v4.4

--- administration2.php
+++ administration2.php
@@ -9,7 +9,7 @@ include ('php/post.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="" />
+    <link rel="shortcut icon" href="img/logo_nws.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
